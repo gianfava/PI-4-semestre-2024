3ad49c4ed3fd7f5d364e54d8c42da972ca70c884
Gian C Fava <gianfava@gmail.com>
2024-11-16 08:51:42 -0300

    Implementação da API em Dash, Ajustes em Login

--- mobile/data-weather/App.js
+++ mobile/data-weather/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Dash from './components/Dash';
-
+import Temperature from './components/Temperature';
+import Humidity from './components/Humidity';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true); // Define que o usuário está logado
+  };
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Login">
+          {() => <Login onLogin={handleLogin} />}
+        </Stack.Screen>
         <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Dash" component={Dash} />
-
+        <Stack.Screen name="Dashboard" component={Dash} />
+        <Stack.Screen name="Temperature" component={Temperature} />
+        <Stack.Screen name="Humidity" component={Humidity} />
       </Stack.Navigator>
     </NavigationContainer>
   );
