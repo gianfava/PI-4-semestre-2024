3dd5558e96678e5204fac7cc4e91491b38292c35
Silvio Junior <juninho.1401@hotmail.com>
2024-11-16 13:10:10 -0300

    Add OpenAPI e CORS - API Estatísticas

--- sensor_estatisticas_api/src/app.js
+++ sensor_estatisticas_api/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+var cors = require("cors");
 const statsRoutes = require("./api/v1/routes/statsRoutes");
 const userRoutes = require("./api/v1/routes/userRoutes");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1/stats", statsRoutes);
 app.use("/api/v1/users", userRoutes);
